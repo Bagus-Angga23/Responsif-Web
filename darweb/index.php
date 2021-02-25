d63cac4fea39c30b94c983dab0e7471ddac78994
Bagus Angga Darmawan <79509397+Bagus-Angga23@users.noreply.github.com>
2021-02-25 22:46:19 +0800

    Add files via upload

---
+++ darweb/index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+<!-- My CSS -->
+<style>
+section {
+    min-height: 420px;
+}
+</style>
+
+    <title>TechIndo</title>
+  </head>
+  <!-- mengatur jarak dari navbar atas -->
+  <body class="mt-5">
+
+<!-- Navbar -->
+  <nav class="navbar fixed-top  text-right navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+    <a class="navbar-brand" href="#">Angga Darmawan</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Tentang</a>
+            <a class="nav-item nav-link" href="#">ArtikelTerbaru</a>
+            <a class="nav-item nav-link disabled" href="#">Contact</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+ <!-- Bootstrap CSS -->
+
+ 
+ <!-- Jumbotron -->
+    <div   class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="asset/img/2.jpg " width="25%" class="rounded-circle img-thumbnail" > <!-- mengatur foto dan menyesuaikan dengan page -->
+            <h1 class="display-4">Bagus Angga Darmawan</h1>
+            <p class="lead">"Memberikan Informasi seputar kemajuan Teknologi"</p>
+        </div>
+    </div>
+    
+
+  <!-- Bootstrap CSS -->
+<section id="about" class="about">
+  <div class="container">
+    <div class="row mb-4">
+    <div class="col text-center">
+  <h2> About </h2>
+  </div>
+  </div>
+
+    <div class="row justify-content-center">
+    <div class="col-md-5 text-justify">
+    <p>Hai.. pada website ini ditujukan kepada para pecinta kemajuan teknologi layaknya smartphone yang setiap tahunya mendapatkan teknologi terbaru maupun update- update yang sangat menarik dan di nanti - nanti oleh pecinta gadget atau gawai.Tentunya ini akan menjadi hal sangat menarik untuk dibahas kedepannya.</p>
+    </div>
+    <div class="col-md-5 text-justify">
+    <p>Kemajuan teknologi dalam bidang smartphone ini patut kita ajungkan jempol , karena hanya dalam beberapa tahun perkembangan smartphone sudah sangat pesat. Smartphone pada saat ini adalah barang yang sepertinya wajib dibawa kemana - mana baik itu sebagai alat komunikasi maupun sebagai alat untuk mengabadikan moment bersama orang - orang terdekat kita.</p>
+    </div>
+    </div>
+  </div>
+  </section>
+
+    <section id="portfolio" class="portfolio bg-light pb-4">
+    <!-- Colom -->
+        <div class="container">
+            <div class="row mb-4 pt-4"> 
+            <div class="col text-center">
+            <h2>Artikel Terbaru</h2>
+        </div>
+    </div>
+
+ <div class="row mb-4 text-justify"> <!-- mengatur jarak antar colom dan merapikan -->
+    <div class="col-md">
+        <div class="card">
+        <img class="card-img-top" src="asset/img/mi11.jpg" alt="Card image cap" >
+            <div class="card-body">
+            <p class="card-text">Xiaomi Mi 11 ditenagai oleh system-on-chip (SoC) Snapdragon 888, teranyar dari Qualcomm. Ponsel flagship Xiaomi ini bisa dibilang sebagai smartphone pertama di dunia yang menggunakan Snapdragon 888.</p>
+            </div>
+            </div>
+        </div>
+<!-- Colom 2 -->
+        <div class="col-md">
+ <div class="card">
+        <img class="card-img-top" src="asset/img/samsung.jpg" alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">Samsung Galaxy S21 Ultra hadir dengan S Pen Stylus, layar Dynamic AMOLED 2X 6,8", quad kamera utama 108MP, kamera selfie 40MP, prosesor Exynos 2100, baterai 5.000 mAh fast charging 25W.</p>
+            </div>
+             </div>
+        </div>
+<!-- Colom 3 -->
+<div class="col-md">
+ <div class="card">
+        <img class="card-img-top" src="asset/img/9t.jpg" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text">Redmi 9T yang memiliki ketebalan 9,6mm dengan bobot 198 gram sebenarnya sudah lebih dulu diperkenalkan secara global pada Januari lalu. Spesifikasi Redmi 9T di Indonesia tak banyak berbeda.</p>
+        </div>
+        </div>
+        </div>
+ </div>
+</div>
+</section>
+
+<!-- Colom contact us -->
+<section id="contact" class="contact mb-5 ">
+<div class="container">
+<div class="row pt-4 mb-4">
+<div class="col text-center">
+<h2>Contact Us</h2>
+</div>
+</div>
+
+<div class="row justify-content-center">
+<div class="col-lg-4">  
+        <div class="card text-white bg-primary mb-3 text-center">
+            <div class="card-body">
+            <h5 class="card-title">Contact Us</h5>
+            <p class="card-text">Jika anda mengalami masalah, anda bisa kontak saya di bawah ini : </p>
+        </div>
+        </div>
+
+                <ul class="list-group">
+        <li class="list-group-item">Location</li>
+        <li class="list-group-item">My Office</li>
+        <li class="list-group-item">Jln. Jalak Putih Banyuasri</li>
+        <li class="list-group-item">Buleleng, Bali</li>
+        </ul>
+</div>
+
+<div class="col-lg-6">
+        <form>
+        <div class="form-group">
+            <label for="Nama">Nama</label>
+            <input type="text" class="form-control" id="Nama">
+       
+        </div>
+        <div class="form-group">
+            <label for="Email">Email</label>
+            <input type="text" class="form-control" id="Email">
+       
+        </div>
+        <div class="form-group">
+            <label for="No Telepon">No Telepon</label>
+            <input type="text" class="form-control" id="No Telepon">
+       
+        </div>
+        <div class="form-group">
+            <label for="Pesan">Pesan</label>
+            <textarea name="Pesan" id="Pesan" class="form-control"></textarea>
+         </div>
+         <div class="form-group">
+        <button type="button" class="btn btn-primary">Kirim Pesan!</button>
+         </div>
+        </form>
+        </div>
+</div>
+
+</div>
+</section>
+
+    <footer class="bg-dark text-white">
+    <div class="container">
+    <div class="row text-center pt-3">
+    <div class="col">
+    <p>Copyright 2021.</p>
+    </div>
+    </div>
+    </div>
+    </footer>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
